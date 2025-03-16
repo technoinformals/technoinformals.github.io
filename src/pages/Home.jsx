d898bcd7d26bec9dcb5a866f40dba4ae737b9c61
Better-Stay-Unknown <85383211+Better-Stay-Unknown@users.noreply.github.com>
2025-03-16 17:51:15 +0530

    Feature/package upgrade (#8)
    * upgrading packages
    
    * fix breaking timeline
    
    * cra -> vite migration
    
    * fix image load - vite don't allow require :(
    
    * remove tsc check for build
    
    * favicon change
    
    * favicon fix
    
    ---------
    
    Co-authored-by: technoinformals <technoinformals20@gmail.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,7 +55,7 @@ export default function Home() {
             {imagesData.map((image, index) => (
               <Image
                 key={index}
-                src={require("../assets/" + image.src)}
+                src={new URL("../assets/" + image.src, import.meta.url).href}
                 hoverData={image.hoverData}
               />
             ))}
